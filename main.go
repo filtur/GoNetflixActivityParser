31f63c6a9c01bb5c65c6e4cdcaca0cfe572e05de
Phil T <cisphil@yahoo.com>
2022-04-02 11:40:19 -0700

    Add console input and simple commands

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -10,5 +13,32 @@ func main() {
 
 	if len(args) == 0 {
 		fmt.Println("Usage: GoNetflixActivityParser <input file path>")
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Enter a command")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		handleInput(scanner.Text(), data)
+	}
+}
+
+func handleInput(input string, data [][]string) {
+	switch input {
+	case "/stats":
+		fmt.Printf("Items watched: %d\n", len(data))
+	case "/quit":
+		os.Exit(3)
 	}
 }
